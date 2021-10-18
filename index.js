25afad53205b915ecd4f329c3e20f202583de0ff
meysam <meisam.design.ph@gmail.com>
2021-10-19 00:38:41 +0330

    complate proje

---
+++ index.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+  const checkbox = document.getElementById("switch");
+  const priceMonth = document.querySelectorAll(".price-month");
+  const priceYear = document.querySelectorAll(".price-year");
+  checkbox.addEventListener("change", function(){
+    changePrice();
+  })
+  function changePrice() {
+    if(checkbox.checked){
+      for (let i = 0; i < priceYear.length; i++) {
+        priceYear[i].style.display = "none";
+        
+      }
+      for (let i = 0; i < priceMonth.length; i++) {
+        priceMonth[i].style.display = "block";
+        
+      }
+    } else {
+      for (let i = 0; i < priceMonth.length; i++) {
+        priceMonth[i].style.display = "none";
+        
+      }
+      for (let i = 0; i < priceYear.length; i++) {
+        priceYear[i].style.display = "block";
+        
+      }
+    }
+  }
+}
